eda6fc9a853557b6baf9581b8fdd18d3a5dee628
Edward <nked1010154@gmail.com>
2024-09-12 01:37:28 +0800

    完成作業

--- text.c
+++ text.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 int main() {
-    char c = '-';
-    printf("%d\n",(c == '+'));
-    printf("%d\n",(c == '-'));
-    printf("%d\n",(c == '*'));
+    char x;
+    int a , b;
+    scanf("%d%c%d" , &a , &x , &b);
+    switch (x) {
+    case '+':
+        printf("%d\n" , a+b)
+        break;
+    case '-':
+        printf("%d\n" , a-b)
+        break;20*300
+    case '*':
+        printf("%d\n" , a*b)
+        break;
+    }
 }
