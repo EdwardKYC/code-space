fb0cddb54bc4c47d5477ebeec29ce0a0a040e45c
EdwardKYC <nked1010154@gmail.com>
2024-09-20 15:29:01 +0800

    寫CESC

--- CSEC/Number_Spiral.cpp
+++ CSEC/Number_Spiral.cpp
@@ -6,10 +6,19 @@ int main () {
     cin.tie(NULL);
     ll n;
     cin >> n;
-    int a , b;
+    ll a , b;
     for (int i = 0; i < n; i++) {
         cin >> a >> b;
-        
+        if (a == b)
+            cout << a*a - a + 1 << '\n';
+        else if (a > b && a % 2 == 0) 
+            cout << a*a - b + 1 << '\n';
+        else if (a > b && a % 2 != 0) 
+            cout << a*a - 2*a + b + 1 << '\n';
+        else if (a < b && b % 2 == 0) 
+            cout << b*b - 2*b + a + 1 << '\n';
+        else if (a < b && b % 2 != 0) 
+            cout << b*b - a + 1 << '\n';
     }
     return 0;
 }
---
+++ CSEC/Two_Sets.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+int main () {
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    ll n;
+    cin >> n;
+    
+    return 0;
+}
