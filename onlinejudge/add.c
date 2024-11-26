66b6d218e177174217fd1f39ee36bf7ae2a4758c
EdwardKYC <nked1010154@gmail.com>
2024-11-27 03:41:54 +0800

    11/27專案時間

---
+++ onlinejudge/add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+//#include <windows.h>
+
+int main() {
+    //SetConsoleOutputCP(CP_UTF8);
+    int n;
+    scanf("%d" , &n);
+    printf("%d" , n);
+    return 0;
+}
---
+++ onlinejudge/add.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pii pair<ll, ll>
+#define vll vector<long long int>
+#define ALL(x) x.begin() , x.end()
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n;
+    scanf("%d" , &n);
+    printf("%d" , n);
+    return 0;
+}
