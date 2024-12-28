282376a2fbb1bf0bc8c08100ad28a085558c528f
EdwardKYC <nked1010154@gmail.com>
2024-12-28 18:22:14 +0800

    五題夠多了

--- CSEC/Grid_Paths.cpp
+++ CSEC/Grid_Paths.cpp
@@ -1,24 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int 
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-char dir[4] = {'D', 'U', 'L', 'R'};
-int ans = 0;
-void dfs(int x , int y , string &s , string target) {
-    for (int i = 0; i < 4; i++) {
-        if (x+dx[i] < 7 || x+dx[i] >= 0 || y+dy[i] < 7 || y+dy[i] >= 0) {
-            if 
-            dfs(x+dx[i] , x+dx[i] >= 0 , s , target);
-        }
-    }
-}
+#define ll long long int
+#define pii pair<int, int>
+#define vll vector<long long int>
+#define ALL(x) x.begin() , x.end()
+const int mod = 1e9 + 7;
+vector< vector<ll> > dp(1001 , vector<ll>(1001 , 0));
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    string target[48] , s[48];
-    cin >> target;
-
-    dfs(0 , 0 , s , target);
+    cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    char c;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> c;
+            if (c == '*') dp[i][j] = -1;
+        }
+    }
+    if (dp[1][1] == -1 || dp[n][n] == -1) {
+        cout << 0;
+        return 0;
+    }
+    dp[1][1] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (dp[i][j] == -1) continue;
+            if (dp[i-1][j] == -1) dp[i][j]++;
+            if (dp[i][j-1] == -1) dp[i][j]++;
+            dp[i][j] += (dp[i-1][j] + dp[i][j-1]) % mod;
+        }
+    }
+    
+    cout << dp[n][n];
     return 0;
 }
