27281e1053d602912632ab13fd3f750afba22695
EdwardKYC <nked1010154@gmail.com>
2025-07-27 06:38:56 +0800

    7/26

---
+++ CSEC/Counting_Towers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pii pair<int, int>
+#define vll vector<long long int>
+#define ALL(x) x.begin() , x.end()
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+       
+    return 0;
+}
